3cd9ec28cdf1e31edbb15e7287ab69e52a3f3f49
Lars Jørgen Solberg <supersolberg@gmail.com>
2015-10-18 21:16:01 +0200

    make a log entry when starting and stopping

--- Source/Navgrid/Private/NavGridPlugin.cpp
+++ Source/Navgrid/Private/NavGridPlugin.cpp
@@ -4,10 +4,12 @@
 
 void NavGridPluginImpl::StartupModule()
 {
+	UE_LOG(NavGrid, Log, TEXT("Starting"));
 }
 
 void NavGridPluginImpl::ShutdownModule()
 {
+	UE_LOG(NavGrid, Log, TEXT("Shutting down"));
 }
 
 IMPLEMENT_MODULE(NavGridPluginImpl, NavGrid)
