ff75170b6fa1ed6bb08c81d2584a130c724340b4
Lars Jørgen Solberg <supersolberg@gmail.com>
2016-04-03 17:30:05 +0200

    rename module interface

---
+++ Source/Navgrid/Public/INavGrid.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "ModuleManager.h"
+
+/**
+* The public interface to this module.  In most cases, this interface is only public to sibling modules
+* within this plugin.
+*/
+class INavGrid: public IModuleInterface
+{
+
+public:
+
+	/**
+	* Singleton-like access to this module's interface.  This is just for convenience!
+	* Beware of calling this during the shutdown phase, though.  Your module might have been unloaded already.
+	*
+	* @return Returns singleton instance, loading the module on demand if needed
+	*/
+	static inline INavGrid& Get()
+	{
+		return FModuleManager::LoadModuleChecked<INavGrid>("NavGrid");
+	}
+
+	/**
+	* Checks to see if this module is loaded and ready.  It is only valid to call Get() if IsAvailable() returns true.
+	*
+	* @return True if the module is loaded and ready to use
+	*/
+	static inline bool IsAvailable()
+	{
+		return FModuleManager::Get().IsModuleLoaded("NavGrid");
+	}
+};
