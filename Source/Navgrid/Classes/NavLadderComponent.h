035add559b3e367b62e6239fef870dbe0a404142
Lars Jørgen Solberg <supersolberg@gmail.com>
2020-04-09 21:00:49 +0200

    move pawn point closer to ladder component

--- Source/Navgrid/Classes/NavLadderComponent.h
+++ Source/Navgrid/Classes/NavLadderComponent.h
@@ -13,7 +13,7 @@ public:
 	UNavLadderComponent();
 
 	virtual void SetGrid(ANavGrid *InGrid) override;
-	virtual FVector GetPawnLocation() const override { return ToWorldSpace(FVector(TileSize / 2, 0, 25)); }
+	virtual FVector GetPawnLocation() const override { return ToWorldSpace(FVector(TileSize / 4, 0, 25)); }
 	virtual void GetNeighbours(const UCapsuleComponent &CollisionCapsule, TArray<UNavTileComponent *> &OutUnObstructed, TArray<UNavTileComponent *> &OutObstructed) override;
 	virtual bool Obstructed(const FVector &FromPos, const UCapsuleComponent &CollisionCapsule) const override;
 	virtual void AddPathSegments(USplineComponent &OutSpline, TArray<FPathSegment> &OutPathSegments, bool EndTile) const override;
@@ -22,7 +22,7 @@ public:
 protected:
 	// local copy of tile ANavGrid::TileSize, value is set in SetGrid()
 	float TileSize;
-	FVector GetTopPathPoint() const { return ToWorldSpace(FVector(TileSize / 2, 0, BoxExtent.Z - 25)); }
-	FVector GetBottomPathPoint() const { return ToWorldSpace(FVector(TileSize / 2, 0, -100)); }
+	FVector GetTopPathPoint() const { return ToWorldSpace(FVector(TileSize / 4, 0, BoxExtent.Z - 25)); }
+	FVector GetBottomPathPoint() const { return ToWorldSpace(FVector(TileSize / 4, 0, -100)); }
 	FVector ToWorldSpace(const FVector &CompSpace) const { return GetComponentLocation() + GetComponentRotation().RotateVector(CompSpace); }
 };
